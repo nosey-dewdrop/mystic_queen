1ff79169b74bdb19fbd223d16b14997917142f42
nosey-dewdrop <damummyphus@gmail.com>
2026-03-25 19:52:46 +0300

    add keychain helper for tamper resistant credit storage

---
+++ MysticQueen/Services/KeychainHelper.swift
@@ -0,0 +1,49 @@
+import Foundation
+import Security
+
+enum KeychainHelper {
+    static func save(_ value: Int, forKey key: String) {
+        let data = withUnsafeBytes(of: value) { Data($0) }
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecAttrService as String: "com.damla.mysticqueen",
+        ]
+
+        // Delete existing
+        SecItemDelete(query as CFDictionary)
+
+        // Add new
+        var addQuery = query
+        addQuery[kSecValueData as String] = data
+        SecItemAdd(addQuery as CFDictionary, nil)
+    }
+
+    static func load(forKey key: String) -> Int? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecAttrService as String: "com.damla.mysticqueen",
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne,
+        ]
+
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+
+        guard status == errSecSuccess, let data = result as? Data, data.count == MemoryLayout<Int>.size else {
+            return nil
+        }
+
+        return data.withUnsafeBytes { $0.load(as: Int.self) }
+    }
+
+    static func delete(forKey key: String) {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecAttrService as String: "com.damla.mysticqueen",
+        ]
+        SecItemDelete(query as CFDictionary)
+    }
+}
